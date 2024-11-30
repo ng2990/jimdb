7c5da3da464c2b8bc5ec01aa8a87d433dd32d690
Aditi Singh <aditisingh@Admins-MacBook-Pro.local>
2024-11-30 10:48:06 -0300

    fix build

--- src/test/java/com/stanrnd/jimdb/Simple.java
+++ src/test/java/com/stanrnd/jimdb/Simple.java
@@ -9,6 +9,7 @@ public class Simple {
     public static void main(String[] args) {
         String s = new String("Abc");
         String u = "bc";
+        System.out.println(u);
         assert(!s.contains(u));
         // This assert failure should be found by JBMC!
     }                           
